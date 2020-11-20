df93c66b6576d2a52f651d4be742d69c0d4f2f8c
Cameron Simbeck <camsimba@ad.unc.edu>
2020-11-20 16:44:01 -0500

    Added backend storage

---
+++ backend.js
@@ -0,0 +1,65 @@
+const express = require('express');
+
+const app = express();
+const User = require('./user.js');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//Gets all users
+app.get('/user', (req, res) => {
+    res.json(User.getAllIDs());
+    return;
+});
+
+//Gets specific user
+app.get('/user/:id', (req, res) => {
+    let user = User.findByID(req.params.id);
+    if (user == null){
+        res.status(404).send("User not found");
+        return;
+    }
+    res.json(user);
+});
+
+//Creates new user
+app.post('/user', (req, res) => {
+    let {name, password, playlist} = req.body;
+    let u = User.create(name, password, playlist);
+    if (u == null){
+        res.status(400).send("Bad Request");
+        return;
+    }
+    return res.json(u);
+});
+
+//Updates info
+app.put('/user/:id', (req, res) =>{
+    let user = User.findByID(req.params.id);
+    if (user == null){
+        res.status(404).send("User not found");
+        return;
+    }
+    let {name, password, playlist} = req.body;
+    user.name = name;
+    user.password = password;
+    user.playlist = playlist;
+
+    user.update();
+    res.json(user);
+});
+
+app.delete('/book/:id', (req, res) =>{
+    let user = User.findByID(req.params.id);
+    if (user == null){
+        res.status(404).send("User not found");
+        return;
+    }
+    user.delete();
+    res.json(true);
+});
+
+const port = 3030;
+app.listen(port, () =>{
+    console.log("backend up and running");
+});
+
---
+++ user.js
@@ -0,0 +1,49 @@
+const user_data = require('data-store')({ path: process.cwd() + '/data/user.json' });
+
+class User {
+
+    constructor(id, name, password, playlist){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.playlist = playlist;
+    }
+
+    update(){
+        user_data.set(this.id.toString(), this);
+    }
+
+    delete(){
+        user_data.del(this.id.toString());
+    }
+}
+
+User.getAllIDs = () =>{
+    //Returns array of all users
+    return Object.keys(user_data.data).map((id => {return parseInt(id);}));
+}
+
+User.findByID = (id) => {
+    let udata = user_data.get(id);
+    if (udata == null){
+        return null;
+    }
+    return new User(udata.id, udata.name, udata.password, udata.playlist);
+}
+
+User.create = (name, password, playlist) =>{
+    let id = User.getAllIDs().reduce((max, next_id) => {
+        if (max < next_id){
+            return next_id;
+        }
+        return max;
+    }, -1) + 1;
+    let u = new User(id, name, password, playlist);
+    user_data.set(u.id.toString(), u);
+    return u;
+}
+
+let u1 = new User(0, 'John', [1,2,3,4]);
+user_data.set(u1.id.toString(), u1);
+
+module.exports = User;
