8ca4497e1f74f92dd95b420862eac7760a639db3
Mattheus Bezerra <mattbeze@live.unc.edu>
2020-11-20 14:53:12 -0500

    Popular Music Playlists Page
    Added a popular music playlists page using the API. Specifically for the search "Hit songs". May need to be modified to allow scrolling or appending new values.

---
+++ popular/script.js
@@ -0,0 +1,32 @@
+$(function(){
+    let api_key = "AIzaSyDrxzwKb_8gmqdWtdiW5_emiFcDYroEa34";
+    let playlist = ''
+
+    videoSearch(api_key, "Hit songs", 10); 
+   
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+       
+        let searchValue = $("#search").val();
+        videoSearch(api_key,searchValue,10);
+    })
+
+
+    function videoSearch(apiKey, searchTerm, maxResults) {
+
+        $("#playlists").empty();
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key="+ apiKey 
+        +"&type=playlist&part=snippet&maxResults=" + maxResults +"&q=" + searchTerm, 
+        function(data){
+            data.items.forEach(item => {
+                playlist = `
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${item.id.playlistId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                `
+                $("#playlists").append(playlist);
+            })
+        });
+    }
+
+});
