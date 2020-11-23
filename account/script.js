98b5974f1e8d8426a6ead817046c3a874da3d783
Cindy Wang <cindywu@Cindys-MacBook-Pro.local>
2020-11-22 20:10:52 -0500

    deleted commented out code

--- account/script.js
+++ account/script.js
@@ -17,81 +17,82 @@ let userID = null;
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-      userID = user.uid;
-      
-      $(".navbar-end").empty();
-      let pages = `
+    userID = user.uid;
+
+    $(".navbar-end").empty();
+    let pages = `
       <a class="navbar-item has-text-weight-bold" href="..#${userID}">Home</a>
-  <a class="navbar-item has-text-weight-bold" href="../popular/#${userID}">Popular Playlists</a>
-  <a class="navbar-item has-text-weight-bold" href="../create/#${userID}">Create a Playlist</a>
-  <a class="navbar-item has-text-weight-bold" href="../account/#${userID}">My Account</a>
+      <a class="navbar-item has-text-weight-bold" href="../popular/#${userID}">Popular Playlists</a>
+      <a class="navbar-item has-text-weight-bold" href="../create/#${userID}">Create a Playlist</a>
+      <a class="navbar-item has-text-weight-bold" href="../account/#${userID}">My Account</a>
       `;
-      $(".navbar-end").append(pages);
+    $(".navbar-end").append(pages);
 
 
   } else {
-      userID = null;
+    userID = null;
   }
+  console.log(userID);
 }
 )
 
-$(async function(){
+$(async function () {
 
-    const removeButtonHandler = function (event) {
-        event.preventDefault();
-        let vidId = event.target.id.substring(4);
-        if (userID != null) {
-            //POST request to playlist of current user
-            remove(vidId, userID).then(() => alert('This video was removed from your playlist!'),
-               () => alert('This video was not removed your playlist :('));
-        } else {
-            alert('Sign up or log in before creating your playlist!');
-        }
+  const removeButtonHandler = function (event) {
+    event.preventDefault();
+    let vidId = event.target.id.substring(4);
+    if (userID != null) {
+      //POST request to playlist of current user
+      remove(vidId, userID).then(() => alert('This video was removed from your playlist!'),
+        () => alert('This video was not removed your playlist :('));
+    } else {
+      alert('Sign up or log in before creating your playlist!');
     }
+  }
 
-    async function remove(vidId, userID) {
+  async function remove(vidId, userID) {
 
-        let user = null;
-        const data = await firestore.collection("users").where('uid', '==', `${userID}`).get();
-        data.forEach(doc => {
-            user = doc.data();
-        })
-        user.playlists.splice(user.playlists.indexOf(vidId),1);
-    
-        firestore.doc(`users/${user.username}`).set(user);
+    let user = null;
+    const data = await firestore.collection("users").where('uid', '==', `${userID}`).get();
+    data.forEach(doc => {
+      user = doc.data();
+    })
+    user.playlists.splice(user.playlists.indexOf(vidId), 1);
 
-        $('#' + vidId).empty();
-    }
+    firestore.doc(`users/${user.username}`).set(user);
 
-    function mainVid(vidID, api){
-        return new Promise((resolve, reject) =>{
-          let vidDiv = $("#vidDiv");
-          vidDiv.empty();
-          $.get('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=' + vidID + '&key=' + api,
-            function(data){
-                video = `
+    $('#' + vidId).empty();
+  }
+
+  function mainVid(vidID, api) {
+    return new Promise((resolve, reject) => {
+      let vidDiv = $("#vidDiv");
+      vidDiv.empty();
+      $.get('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=' + vidID + '&key=' + api,
+        function (data) {
+          video = `
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/${data.items[0].id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 `
-                vidDiv.append(video);
-            }
-          ).then(() => resolve());
-        })
-        
-      
-          
-      }
-      
-      function injectPlaylist(playlist,api){
-        let vidDiv = $("#vidDiv");
-      
-        playlist.forEach(videoID => {
-          $.get('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=' + videoID + '&key=' + api,
-          function(data){
-            let title = data.items[0].snippet.title;
-            let thumbnail = data.items[0].snippet.thumbnails.default.url;  
-            let desc = data.items[0].snippet.description;
-      
-            let vid = `
+          vidDiv.append(video);
+        }
+      ).then(() => resolve());
+    })
+
+
+
+  }
+
+  function injectPlaylist(playlist, api) {
+    let vidDiv = $("#vidDiv");
+
+    playlist.forEach(videoID => {
+      $.get('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=' + videoID + '&key=' + api,
+        function (data) {
+          let title = data.items[0].snippet.title;
+          let thumbnail = data.items[0].snippet.thumbnails.default.url;
+          let desc = data.items[0].snippet.description;
+
+          let vid = `
             <div id=${data.items[0].id}>
             <img src="${thumbnail}" alt="" class="thumb">
             <h2><strong>${title}</strong></h2>
@@ -100,23 +101,23 @@ $(async function(){
             </div>
           `;
           vidDiv.append(vid);
-          });
-        })
-      }
+        });
+    })
+  }
 
 
 
-    let userID = window.location.href;
-    userID = userID.substring(userID.indexOf('#') + 1);
-    let playlist;
-    const data = await firestore.collection("users").where('uid', '==', `${userID}`).get();
-    data.forEach(doc => {
-        playlist = doc.data().playlists;
-    });
-    if(playlist.length == 0){
-        $("#topcontainer").append($('<h1>You do not have any saved videos</h1>'));
-    }
-    mainVid(playlist[0], firebaseConfig.apiKey).then(()=> injectPlaylist(playlist, firebaseConfig.apiKey));
+  let userID = window.location.href;
+  userID = userID.substring(userID.indexOf('#') + 1);
+  let playlist;
+  const data = await firestore.collection("users").where('uid', '==', `${userID}`).get();
+  data.forEach(doc => {
+    playlist = doc.data().playlists;
+  });
+  if (playlist.length == 0) {
+    $("#topcontainer").append($('<h1>You do not have any saved videos</h1>'));
+  }
+  mainVid(playlist[0], firebaseConfig.apiKey).then(() => injectPlaylist(playlist, firebaseConfig.apiKey));
 
-    $(document).on('click', ".del", removeButtonHandler);
+  $(document).on('click', ".del", removeButtonHandler);
 })
--- create/script.js
+++ create/script.js
@@ -34,55 +34,10 @@ firebase.auth().onAuthStateChanged((user) => {
     }
 }
 )
-/*var firebaseConfig = {
-    apiKey: "AIzaSyA7kpv-iyaIVtcmmys_eIekAvRMzM7OIkw",
-    authDomain: "redredistribution.firebaseapp.com",
-    databaseURL: "https://redredistribution.firebaseio.com",
-    projectId: "redredistribution",
-    storageBucket: "redredistribution.appspot.com",
-    messagingSenderId: "5265188128",
-    appId: "1:5265188128:web:26327e636df2f57e01e345",
-    measurementId: "G-30Z3SHKGLM"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-let firestore = firebase.firestore();
-
-let userID = null;
-
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        userID = user.uid;
-    } else {
-        userID = null;
-    }
-}
-)*/
 
 // adds new song to playlist
-// is the link right? do i understand how axios requests work? so many questions to ask
 async function add(vidId, userID) {
-    // let user = null;
-    // // const data = await firestore.collection("users").where('uid', '==', `${userID}`);
-    // const docRef = firestore.collection("users").get().then(function(value){
-    //     value.forEach(function(doc){
-    //       let strUsername = doc.data().uid;
-    //       if (strUsername == userID) {
-    //           user = doc;
-    //       }
-    //     });
-    // console.log("here! " + user);
-
-    // data.forEach(doc => {
-    //     user = doc.data();
-    // })
-
-    // user.playlists.push(vidId); 
-
-    // await firestore.doc(`users/${user.username}`).set(user);
-    //   });
-
     let user = null;
     const data = await firestore.collection("users").where('uid', '==', `${userID}`).get();
     data.forEach(doc => {
