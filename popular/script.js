1d21c99c587d2917390509685780c87af13aea50
Mattheus Bezerra <mattbeze@live.unc.edu>
2020-11-22 19:58:55 -0500

    Page Transfers

--- popular/script.js
+++ popular/script.js
@@ -1,3 +1,40 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA7kpv-iyaIVtcmmys_eIekAvRMzM7OIkw",
+  authDomain: "redredistribution.firebaseapp.com",
+  databaseURL: "https://redredistribution.firebaseio.com",
+  projectId: "redredistribution",
+  storageBucket: "redredistribution.appspot.com",
+  messagingSenderId: "5265188128",
+  appId: "1:5265188128:web:26327e636df2f57e01e345",
+  measurementId: "G-30Z3SHKGLM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let firestore = firebase.firestore();
+
+let userID = null;
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+      userID = user.uid;
+      
+      $(".navbar-end").empty();
+      let pages = `
+      <a class="navbar-item has-text-weight-bold" href="..#${userID}">Home</a>
+  <a class="navbar-item has-text-weight-bold" href="../popular/#${userID}">Popular Playlists</a>
+  <a class="navbar-item has-text-weight-bold" href="../create/#${userID}">Create a Playlist</a>
+  <a class="navbar-item has-text-weight-bold" href="../account/#${userID}">My Account</a>
+      `;
+      $(".navbar-end").append(pages);
+
+
+  } else {
+      userID = null;
+  }
+}
+)
+
 
 $(function(){
   var firebaseConfig = {
