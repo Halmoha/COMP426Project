05aa4f8dee6cf1145d90b8ddbf313167d9c320cc
Cindy Wang <cindywu@Cindys-MacBook-Pro.local>
2020-11-23 22:19:03 -0500

    added documentation and made some corrections to home navbar

--- create/script.js
+++ create/script.js
@@ -19,6 +19,7 @@ firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         userID = user.uid;
         
+        // when the user is sign in, the url's will all be updated to include their userID
         $(".navbar-end").empty();
         let pages = `
         <a class="navbar-item has-text-weight-bold" href="..#${userID}">Home</a>
@@ -36,26 +37,14 @@ firebase.auth().onAuthStateChanged((user) => {
 )
 
 
-// adds new song to playlist
-async function add(vidId, userID) {
-    let user = null;
-    const data = await firestore.collection("users").where('uid', '==', `${userID}`).get();
-    data.forEach(doc => {
-        user = doc.data();
-    })
-    console.log(user);
-
-    user.playlists.push(vidId);
-
-    firestore.doc(`users/${user.username}`).set(user);
-
-}
-
+//
+// event handler for adding a new song to the user's playlist
+//
 const handleAddButtonPress = function (event) {
     event.preventDefault();
     let vidId = this.id.substring(10);
     if (userID != null) {
-        //POST request to playlist of current user
+        // if the user is signed in, add the song to the user's playlist
         add(vidId, userID).then(() => alert('This video was added to your playlist!'),
             () => alert('This video was not added to your playlist :('));
     } else {
@@ -63,17 +52,38 @@ const handleAddButtonPress = function (event) {
     }
 }
 
+//
+// adds new song to playlist
+//
+async function add(vidId, userID) {
+    let user = null;
+
+    // use firestore.collection to find the user with the current userID
+    const data = await firestore.collection("users").where('uid', '==', `${userID}`).get();
+    data.forEach(doc => {
+        user = doc.data();
+    })
+
+    // push the video ID to the user's playlist
+    user.playlists.push(vidId);
+
+    // update the user's data in firestore
+    firestore.doc(`users/${user.username}`).set(user);
+}
 
 $(function () {
 
+    // youtube API key
     let api_key = "AIzaSyAk0dzxvfOC31bFONZV50NXpjgGOTfZMZ4";
     let video = ''
     const $videos = $("#videos");
 
+    // screen automatically shows 10 results from a "Music" search
     videoSearch(api_key, "Music", 10);
 
 
 
+    // when the search form is submitted, 10 results from the searchValue will be shown
     $("form").submit(function (event) {
         event.preventDefault();
 
@@ -84,10 +94,12 @@ $(function () {
     $videos.on("click", "button.add", handleAddButtonPress);
 
 
+    // returns results from a YouTube search for a certain searchTerm
     function videoSearch(apiKey, searchTerm, maxResults) {
 
         $videos.empty();
 
+        // use YouTube API to search videos from the database and display them
         $.get("https://www.googleapis.com/youtube/v3/search?key=" + apiKey
             + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + searchTerm,
             function (data) {
